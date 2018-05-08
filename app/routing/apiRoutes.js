14fcd11b3fa9c3ff73a182e7f0d3f8a0bd6eb70b
Kyle Weingart <kyle.weingart@gmail.com>
2018-05-08 14:01:12 -0400

    minor updates

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,6 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a "data" source.
-// This data source hold arrays of information on friends
+
 // ===============================================================================
 
  var friends = require("../data/friends.js");
@@ -14,9 +13,7 @@
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+  
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
@@ -26,20 +23,15 @@ module.exports = function(app) {
   
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In the below case, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the friends JavaScript array
-  // (ex. User fills out a friend survey... this data is then sent to the server...
-  // Then the server saves the data to the friendData array)
+  
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know who there best friend match is.
-    // req.body is available since we're using the body-parser middleware
+  //  logic to find best friend match
 
 
 
-      
+    
       
       var bestMatch = {
         name: "",
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,8 +11,7 @@ var path = require("path");
 
 module.exports = function(app) {
   // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
+
   // ---------------------------------------------------------------------------
 
   app.get("/survey", function(req, res) {
